886403a52b7c7e4102ed7cc169513188cfbad318
Justin Kamper <justinjkamper@gmail.com>
2026-06-13 23:48:05 +0100

    Added coordinate mapping by (ht, row, col) key.

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ from HouseNum import HouseNum
 from HouseType import HouseType
 
 
-def get_house_cell_coords(house_type: HouseType, house_num: HouseNum) -> list[tuple[int, int]]:
+def get_coords_for_house(house_type: HouseType, house_num: HouseNum) -> list[tuple[int, int]]:
     idx = house_num.value - 1
 
     if house_type == HouseType.ROW:
@@ -16,5 +16,18 @@ def get_house_cell_coords(house_type: HouseType, house_num: HouseNum) -> list[tu
         start_col = (idx % 3) * 3
         return [(start_row + r, start_col + c) for r in range(3) for c in range(3)]
 
+def get_house_coords_for_cell_by_type(house_type: HouseType, row: int, col: int) -> list[tuple[int, int]]:
+    if house_type == HouseType.ROW:
+        return [(row, c) for c in range(9)]
 
-HOUSE_CELL_COORD_MAP = {(ht, hn): get_house_cell_coords(ht, hn) for ht in HouseType for hn in HouseNum}
+    elif house_type == HouseType.COL:
+        return [(r, col) for r in range(9)]
+
+    elif house_type == HouseType.BOX:
+        start_row = (row // 3) * 3
+        start_col = (col // 3) * 3
+        return [(start_row + r, start_col + c) for r in range(3) for c in range(3)]
+
+
+HOUSE_COORDS_MAP = {(ht, hn): get_coords_for_house(ht, hn) for ht in HouseType for hn in HouseNum}
+CELL_HOUSE_COORDS_MAP = {(ht, row, col): get_house_coords_for_cell_by_type(ht, row, col) for ht in HouseType for row in range(9) for col in range(9)}
