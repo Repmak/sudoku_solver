1f5283c12132a8ed2a484cc277fb07f1709c7277
Justin Kamper <justinjkamper@gmail.com>
2026-06-14 16:11:01 +0100

    test_board_not_set_up implemented

---
+++ tests/solver_test.py
@@ -0,0 +1,32 @@
+import unittest
+from solver import SudokuSolver
+
+
+GIVENS = ""
+
+class TestSum(unittest.TestCase):
+
+    def test_board_not_set_up(self):
+        solver = SudokuSolver()
+        with self.assertRaises(ValueError):
+            solver.solve()
+
+    # def board_set_up_board(self):
+    #     expected = '''
+    #         \n
+    #         \n
+    #         \n
+    #         \n
+    #         \n
+    #         \n
+    #         \n
+    #         \n
+    #
+    #     '''
+    #     solver = SudokuSolver()
+    #     solver.setup_board(GIVENS)
+    #     self.assertEqual(str(solver), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
